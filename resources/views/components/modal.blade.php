28d148255ce15c3fdf8879e1f60596416ed2a495
Mauricio Perea Rodríguez <mperea0@misena.edu.co>
2021-04-02 08:36:09 -0500

    Added modal component file

---
+++ resources/views/components/modal.blade.php
@@ -0,0 +1,13 @@
+@props([
+    'trigger'
+])
+<div
+    x-show="{{ $trigger }}"
+    x-on:click.self="{{ $trigger }} = false"
+    x-on:keydown.escape.window="{{ $trigger }} = false"
+    class="flex fixed top-0 w-full h-full bg-gray-900 bg-opacity-60 items-center"
+>
+    <div {{ $attributes->merge(['class' => 'm-auto bg-gray-200 shadow-2xl rounded-xl p-8' ]) }} class=>
+        {{ $slot }}
+    </div>
+</div>
